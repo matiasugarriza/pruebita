25071695c88919ec935dbe91c78ba1adb2799ecc
Matias Ugarriza <matiasugarriza@gmail.com>
2022-11-28 19:55:04 -0300

    amorcito

---
+++ main.js
@@ -0,0 +1,7 @@
+let mai = prompt("Ingrese su nombre: ");
+
+if(mai == "Mai" || mai == "Mailen" || mai == "Mailén Trinta" || mai == "mailen" || mai == "mailén" || mai == "mai" || mai == "mailen trinta"){
+    alert("❤ Hola, mi amor, sos la más hermosa. Te amo con todo mi corazón y con toda mi alma. ❤")
+}else{
+    alert("Usted no es mi amorcito.")
+};
